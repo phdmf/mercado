dbf3ae5e40624db189e741946404ba0b6f54f896
phdmf <phdmf@cin.ufpe.br>
2018-11-20 08:37:30 -0300

    Update - Fornecedores: Básica e Array

---
+++ dados/RepositorioFornecedoresArray.java
@@ -0,0 +1,22 @@
+package Fornecedores;
+
+public class repositorioFornecedoresArray implements fornecedor {
+	
+	
+	private String produto [];
+	private Double preco [];
+	//private 
+	
+	
+	public repositorioFornecedoresArray (int tamanho) {
+		fornecedor = new fornecedor [tamanho];
+
+		public repositorioFornecedoresArray() {
+			fornecedores = new fornecedor[30];
+	}
+ 	public void inserir(Concessionaria concessionaria) {
+		concessionarias[indice] = concessionaria;
+		indice++;
+	}
+
+}
