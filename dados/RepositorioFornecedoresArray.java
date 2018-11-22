dc75465e3af7eb107c9c670f0c613eabf007148a
lp9luan <45181359+lp9luan@users.noreply.github.com>
2018-11-22 15:51:21 -0200

    Update RepositorioFornecedoresArray.java

--- dados/RepositorioFornecedoresArray.java
+++ dados/RepositorioFornecedoresArray.java
@@ -1,22 +1,79 @@
 package Fornecedores;
 
-public class repositorioFornecedoresArray implements fornecedor {
-	
-	
-	private String produto [];
-	private Double preco [];
-	//private 
-	
-	
-	public repositorioFornecedoresArray (int tamanho) {
-		fornecedor = new fornecedor [tamanho];
+import negocio.repositorioFornecedores;
 
-		public repositorioFornecedoresArray() {
-			fornecedores = new fornecedor[30];
+public class repositorioFornecedoresArray implements repositorioFornecedores {
+
+	private fornecedor[] fornecedores;
+	private int indice;
+
+	public repositorioFornecedoresArray(int tam) {
+		fornecedores = new fornecedor[tam];
+		indice = 0;
 	}
- 	public void inserir(Concessionaria concessionaria) {
-		concessionarias[indice] = concessionaria;
-		indice++;
+
+	public void inserir(fornecedor f) {
+		fornecedores[indice] = f;
+		indice = indice + 1;
+	}
+
+	public void atualizar(fornecedor cnpj) {// throws ProdutoNaoEncontradoException {
+		boolean achou = false;
+		for (int i = 0; i < indice; i++) {
+			if (fornecedores[i].getCnpj().equals(fornecedores.equals(cnpj))) {
+				fornecedores[i] = fornecedores[i];
+				achou = true;
+			}
+		}
+		if (!achou) {
+			// throw new ProdutoNaoEncontradoException() ;
+		}
+	}
+
+	public fornecedor procurar(String cnpj) { // throws PessoaNaoEncontradaException {
+		fornecedor fornecedores = null;
+		for (int i = 0; i < indice; i++) {
+			if (this.fornecedores[i].getCnpj().equals(cnpj)) {
+				this.fornecedores = fornecedor[i];
+			}
+		}
+		if (this.fornecedores != null) {
+			return fornecedores;
+		} else {
+			// throw new PessoaNaoEncontradaException();
+		}
+	}
+
+	public void remover(String cnpj) { // throws ProdutoNaoEncontradoException {
+		boolean achou = false;
+		for (int i = 0; i < indice; i++) {
+			if (fornecedores[i].getCnpj().equals(cnpj)) {
+				achou = true;
+				fornecedores[i] = null;
+				int j = i;
+				for (i = j; i < indice; i++) {
+					if (i != indice - 1) {
+						fornecedores[i] = fornecedores[i + 1];
+					} else {
+						fornecedores[i] = null;
+						indice--;
+					}
+				}
+			}
+		}
+		if (!achou) {
+			// throw new ProdutoNaoEncontradoException() ;
+		}
+	}
+
+	public boolean existe(String cnpj) {
+		boolean achou = false;
+		for (int i = 0; i < indice; i++) {
+			if (fornecedores[i].getCnpj().equals(cnpj)) {
+				achou = true;
+			}
+		}
+		return achou;
 	}
 
 }
