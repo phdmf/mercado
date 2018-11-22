0ff804e7ddf505a38b9bbc761b80b64ba25b36a5
lp9luan <45181359+lp9luan@users.noreply.github.com>
2018-11-22 16:55:29 -0200

    Update repositorioFornecedores.java

--- negocio/repositorioFornecedores.java
+++ negocio/repositorioFornecedores.java
@@ -1,12 +1,20 @@
 package negocio;
 
+import Excessoes.FornecedorDadosIncompletosException;
+import Excessoes.FornecedorInexistenteConsultaException;
+import Excessoes.FornecedorInexistenteRemoveException;
+import Excessoes.FornecedorNaoEncontradoAtException;
+import Excessoes.FornecedorNaoEncontradoPrException;
+import Excessoes.FornecedorNomeJaCadastradoException;
 import Fornecedores.fornecedor;
 
 public interface repositorioFornecedores {
 	
-	public void inserir(fornecedor fornecedor); // criar os casos de erros para habilitar os PNEExceptions. 
-	public void atualizar (fornecedor fornecedor); //throws PNEException;
-	public void remover (String cnpj); // throws PNEException; // o cnpj sera o indice exclusivo (por ser unico) para excluir o fornecedor inteiro
-	public fornecedor procurar (String cnpj); // throws PNEException;
+	public void inserir(fornecedor fornecedor) throws FornecedorNomeJaCadastradoException; 
+	public void atualizar (fornecedor fornecedor) throws FornecedorNaoEncontradoAtException; 
+	public fornecedor procurar (String cnpj) throws FornecedorNaoEncontradoPrException;  
+	boolean existe (String cnpj) throws FornecedorInexistenteConsultaException; 
+	public void remover (String cnpj) throws FornecedorDadosIncompletosException, FornecedorInexistenteRemoveException;
+	// o cnpj sera o indice exclusivo (por ser unico) para excluir o fornecedor inteiro
 
 }
