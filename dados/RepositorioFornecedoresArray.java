19dfe3b04a041b863336bf9dc85b06468d2baca7
lp9luan <45181359+lp9luan@users.noreply.github.com>
2018-11-26 20:09:48 -0200

    Update RepositorioFornecedoresArray.java

--- dados/RepositorioFornecedoresArray.java
+++ dados/RepositorioFornecedoresArray.java
@@ -1,83 +1,98 @@
 package Fornecedores;
 
 import Excessoes.FornecedorDadosIncompletosException;
-import Excessoes.FornecedorNaoEncontradoException;
+import Excessoes.FornecedorInexistenteConsultaException;
 import Excessoes.FornecedorNaoEncontradoException;
 import Excessoes.FornecedorNomeJaCadastradoException;
+import negocio.repositorioFornecedores;
 
-public class repositorioFornecedorLista {
+public class repositorioFornecedoresArray implements repositorioFornecedores {
 
-	class Lista {
-		private fornecedor fornecedor;
-		private Lista proximo;
+	private fornecedor[] fornecedores;
+	private int indice;
 
-		Lista() {
-			this.fornecedor = null;
-			this.proximo = null;
-		}
-
-		// negocios cadastro (checa se tem ou não antes de ir pros metodos);
-		//
-
-		public void inserir(fornecedor forn) throws FornecedorNomeJaCadastradoException { // todo q for criado vai ter
-			if (!existir(forn)) { // forn. e novo fornecedor
-				if (this.fornecedor.equals(null)) {
-					this.fornecedor = forn;
-					this.proximo = new Lista();
-				} else {
-					this.proximo.inserir(forn);
-				}
-			} else {
-				throw new FornecedorNomeJaCadastradoException();
-			}
-		}
-
-		public fornecedor procurar(String cnpj) throws FornecedorNaoEncontradoException { 
-			if (this.fornecedor.getCnpj().equals(cnpj)) {
-				return fornecedor;
-			} else if (this.proximo != null) {
-				return this.proximo.procurar(cnpj);
-			} else {
-				throw new FornecedorNaoEncontradoException();
-			}
-		}
-
-		public boolean existir(fornecedor cnpj) {
-			if (this.fornecedor.getCnpj().equals(cnpj)) {
-				return true;
-			} else if (this.proximo != null) {
-				return this.proximo.existir(cnpj);
-			} else {
-				return false;
-			}
-		}
-
-		// nesse atualizar tem que criar um metodo no main para mudar x para y, por
-		// exemplo
-
-		public void atualizar(fornecedor novo) throws FornecedorNaoEncontradoException {
-			if (this.fornecedor.getCnpj().equals(novo.getCnpj())) {
-				this.fornecedor = novo;
-			} else if (this.proximo != null) {
-				this.proximo.atualizar(novo);
-			} else {
-				throw new FornecedorNaoEncontradoException();
-			}
-		}
-
-		
-		public void remover(fornecedor forn) throws FornecedorNaoEncontradoException {
-			if (this.fornecedor.getCnpj().equals(forn.getCnpj())) {
-				this.fornecedor = this.proximo.fornecedor;
-				this.proximo = this.proximo.proximo; // nessa linha ele gera um loop ate chegar no null?
-
-			} else if (this.proximo != null) {
-				this.proximo.remover(forn);
-			} else {
-				throw new FornecedorNaoEncontradoException();
-			}
-		}
-
+	public repositorioFornecedoresArray(int tam) {
+		fornecedores = new fornecedor[tam];
+		indice = 0;
 	}
 
+	public void inserir(fornecedor forn) throws FornecedorNomeJaCadastradoException { // ta apresentando erro
+		if (!existir(forn)) {
+			fornecedores[indice] = forn;
+			indice = indice + 1;
+		} else {
+			throw new FornecedorNomeJaCadastradoException();
+		}
+	}
+
+	// Classes com Letra maiuscula ***************
+
+	public void atualizar(fornecedor forn) throws FornecedorNaoEncontradoException {
+		boolean achou = false;
+		for (int i = 0; i < indice; i++) {
+			if (fornecedores[i].getCnpj().equals(forn.getCnpj())) {
+				fornecedores[i] = forn;
+				achou = true;
+			}
+		}
+		if (!achou) {
+			throw new FornecedorNaoEncontradoException();
+		}
+	}
+
+	public fornecedor procurar(String cnpj) throws FornecedorNaoEncontradoException { // assinatura do metodo
+		for (int i = 0; i < indice; i++) {
+			if (this.fornecedores[i].getCnpj().equals(cnpj)) {
+				return this.fornecedores[i];
+			}
+		}
+		throw new FornecedorNaoEncontradoException(); // n entendi
+	}
+
+	public void remover(fornecedor forn) throws FornecedorNaoEncontradoException {
+		boolean achou = false;
+		for (int i = 0; i < indice; i++) {
+			if (fornecedores[i].getCnpj().equals(forn.getCnpj())) {
+				achou = true;
+				fornecedores[i] = null;
+				int j = i;
+				for (i = j; i < indice; i++) {
+					if (i != indice - 1) {
+						fornecedores[i] = fornecedores[i + 1];
+					} else {
+						fornecedores[i] = null;
+						indice--;
+					}
+				}
+			}
+		}
+		if (!achou) {
+			throw new FornecedorNaoEncontradoException();
+		}
+	}
+
+	public boolean existir(fornecedor forn) {
+		boolean achou = false;
+		for (int i = 0; i < indice; i++) {
+			if (fornecedores[i].getCnpj().equals(forn.getCnpj())) {
+				achou = true;
+			}
+		}
+		return achou;
+	}
+
+	private int getIndice(fornecedor forn) { // ta ok essa logica. OK
+		String n;
+		boolean achou = false;
+		int i = 0;
+		while ((!achou) && (i < this.indice)) {
+			n = fornecedores[i].getCnpj();
+			if (n.equals(forn.getCnpj())) {
+				achou = true;
+			} else {
+				i = i + 1;
+			}
+		}
+		return i;
+	}
 }
