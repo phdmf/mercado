5fe755b4a7113e9cc64209415130299b22d12089
lp9luan <45181359+lp9luan@users.noreply.github.com>
2018-11-27 19:41:56 -0200

    Update and rename repositorioproduto.java to RepositorioProduto.java

---
+++ Mercadinho/interface/RepositorioProduto.java
@@ -0,0 +1,19 @@
+package Mercadinho.Sointerfaces;
+
+import Mercadinho.basic.Produto;
+import Mercadinho.exception.ProdutoNaoEncontradoException;
+
+public interface RepositorioProduto {
+
+	void inserir(Produto produto);
+
+	Produto procurar(String tipo) throws ProdutoNaoEncontradoException;
+
+	void atualizar(Produto produto) throws ProdutoNaoEncontradoException;
+
+	void remover(String tipo) throws ProdutoNaoEncontradoException;
+
+	boolean existe(String tipo);
+
+	int temQuantos(String produto, int quantidade);
+}
