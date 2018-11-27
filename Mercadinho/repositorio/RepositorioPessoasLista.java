3736ab4a97de0161ab660498606fac61fc9f3c41
lp9luan <45181359+lp9luan@users.noreply.github.com>
2018-11-27 19:48:57 -0200

    Update RepositorioPessoasLista.java

--- Mercadinho/repositorio/RepositorioPessoasLista.java
+++ Mercadinho/repositorio/RepositorioPessoasLista.java
@@ -1,84 +1,112 @@
 package Mercadinho.repositorio;
 
-import basicas.Pessoa;
-import excecoes.PessoaNaoEncontradaException;
-import negocio_dados.RepositorioPessoas;
+import Mercadinho.Sointerfaces.RepositorioPessoas;
+import Mercadinho.basic.Pessoa;
+import Mercadinho.exception.*;
 
-public class RepositorioPessoasLista implements RepositorioPessoas{
-	
-	private Pessoa pessoa;
-	private RepositorioPessoasLista next;
-	
-	public RepositorioPessoasLista() {
-		pessoa = null;
-		next = null;
+public class RepositorioPessoasArray implements RepositorioPessoas {
+
+	private Pessoa[] pessoas;
+	private int indice;
+
+	public RepositorioPessoasArray(int tam) {
+		pessoas = new Pessoa[tam];
+		indice = 0;
 	}
-	
-	public void inserir (Pessoa pessoa) {
-		if (this.pessoa == null) {
-			this.pessoa = pessoa;
-			next = new RepositorioPessoasLista();
-		}
-		else {
-			this.next.inserir(pessoa);
-		}
+
+	public void inserir(Pessoa pessoa) {
+		pessoas[indice] = pessoa;
+		indice = indice + 1;
 	}
-	
-	public Pessoa procurar(String cpf) throws PessoaNaoEncontradaException { 
-		Pessoa pessoa = null;
-		if (this.pessoa != null) {
-			if(this.pessoa.getCpf().equals(cpf)) {
-				pessoa = this.pessoa;
-			}
-			else {
-				this.next.procurar(cpf);
-			}
-		}
-		if (this.pessoa == null) {
-			throw new PessoaNaoEncontradaException();
-		}else {
-			return pessoa;
-		}
-	}
-	
-	public void atualizar (Pessoa pessoa) throws PessoaNaoEncontradaException{
-		if (this.pessoa != null) {
-			if (this.pessoa.getCpf().equals(pessoa.getCpf())) {
-				this.pessoa = pessoa;
-			}
-			else {
-				this.next.atualizar(pessoa);
+
+	// MÃ©todo de *procurar* aplicando o "getIndice" diretamente :D
+	public Pessoa procurar(String cpf) throws PessoaNaoEncontradaException {
+		Pessoa p = null;
+
+		for (int i = 0; i < indice; i++) {
+			if (pessoas[i].getCpf().equals(cpf)) {
+				p = pessoas[i];
 			}
 		}
-		else {
+		if (p != null) {
+			return p;
+		} else {
 			throw new PessoaNaoEncontradaException();
 		}
 	}
-	public void remover (String cpf) throws PessoaNaoEncontradaException{
-		if (this.pessoa != null) {
-			if (this.pessoa.getCpf().equals(cpf)) {
-				this.pessoa = this.next.pessoa;
-				this.next = this.next.next;
-			}
-			else {
-				this.next.remover(cpf);
-			}
-		}
-		else {
-			throw new PessoaNaoEncontradaException();
-		}
-	}
-	
-	public boolean existe (String cpf) {
+
+	// MÃ©todo remover aplicando o "getIndice" diretamente :D
+	public void remover(String cpf) throws PessoaNaoEncontradaException {
 		boolean find = false;
-		if (this.pessoa.getCpf().equals(cpf)) {
-			find = true;
+		for (int i = 0; i < indice; i++) {
+			if (pessoas[i].getCpf().equals(cpf)) {
+				indice = indice - 1;
+				pessoas[i] = pessoas[indice];
+				pessoas[i] = null;
+				find = true;
+			}
 		}
-		else {
-			this.next.existe(cpf);
+		if (!find) {
+			throw new PessoaNaoEncontradaException();
+		}
+	}
+
+	public void atualizar(Pessoa pessoa) throws PessoaNaoEncontradaException {
+		boolean find = false;
+		for (int i = 0; i < indice; i++) {
+			if (pessoas[i].getCpf().equals(pessoa.getCpf())) {
+				pessoas[i] = pessoa;
+				find = true;
+			}
+		}
+		if (!find) {
+			throw new PessoaNaoEncontradaException();
+		}
+	}
+
+	public boolean existir(String cpf) {
+		boolean find = false;
+		for (int i = 0; i < indice; i++) {
+			if (pessoas[i].getCpf().equals(cpf)) {
+				find = true;
+			}
 		}
 		return find;
 	}
-	
+
+	// Exemplo do Projeto feito por SÃ©rgio. Decidir se uso ou nÃ£o no buscar,
+	// atualizar e remover
+	/*
+	 * private int getIndice(String cpf) { String c; boolean find = false; int i =
+	 * 0; while ((!find) && (i < this.indice)) { c = pessoas[i].getCpf();
+	 * if(c.equals(cpf)) { find = true; } else { i = i + 1; } } return i; }
+	 */
+
+	/*
+	 * MÃ©todo de procurar com o getIndice
+	 * 
+	 * public Pessoa procurar (String cpf) throws PNEException { Pessoa p = null;
+	 * int i = this.getIndice(cpf);
+	 * 
+	 * if (i == this.indice) { throw new PNEException(); }else { p =
+	 * this.pessoas[i]; } return p; }
+	 */
+
+	/*
+	 * MÃ©todo remover com o getIndice
+	 * 
+	 * public void remover(String cpf) throws PNEException{ int i =
+	 * this.getIndice(cpf); if (i == this.indice) { throw new PNEException(); }else
+	 * { this.indice = this.indice - 1; this.pessoas[i] = this.pessoas[this.indice];
+	 * this.pessoas[this.indice] = null; } }
+	 */
+
+	/*
+	 * MÃ©todo atualizar com o getIndice
+	 * 
+	 * public void atualizar(Pessoa pessoa) throws PNEException{ int i =
+	 * this.getIndice(pessoa.getCpf()); if(i == this.indice) { throw new
+	 * PNEException(); }else { this.pessoas[i] = pessoa; }
+	 */
 
 }
