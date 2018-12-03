3fb1c7c60eea191b0e8602881ed3ca53e601424f
jgpp <38386145+jguilhermepaiva@users.noreply.github.com>
2018-12-03 17:40:05 -0200

    Add files via upload

---
+++ Produto/Produto/testee.java
@@ -0,0 +1,18 @@
+package Mercadinho.basic;
+
+public class testee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Produto biscoito = new Produto ("Oreo", "Comida", 3.50, 000001, "Marilan");
+		Produto detergente = new Produto ("Limpol", "Limpeza", 2.10, 000002, "Limpol");
+		Produto macarrao = new Produto ("Miojo", "Comida", 1.50, 000003, "Nissin");
+		Produto salgadinho = new Produto ("Doritos", "Comida", 3.20, 000004, "Elma Chips");
+		Produto amendoim = new Produto ("Crokissimo", "Comida", 1.00, 000005, "Santa Helena");
+		Produto vinho = new Produto ("Casal Garcia", "Bebida", 63.0,000006, "Casal Garcia");
+		Produto cerveja = new Produto ("Budweiser", "Bebida", 3.2, 000007, "Anheuser-Busch");
+		Produto cha = new Produto ("Ice Tea", "Bebida", 2.50, 000010, "Leao Fuze");
+
+	}
+
+}
