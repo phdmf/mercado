34bfaeb34514ab3c838df2ba9164d7437a3cf98d
lp9luan <45181359+lp9luan@users.noreply.github.com>
2018-12-03 15:02:27 -0200

    Update RepositorioFornecedorLista.java

--- Fornecedor/RepositorioFornecedorLista.java
+++ Fornecedor/RepositorioFornecedorLista.java
@@ -2,92 +2,72 @@ package Fornecedor;
 
 public class RepositorioFornecedorLista implements RepositorioFornecedores {
 
-		private Fornecedor fornecedor;
-		private RepositorioFornecedorLista proximo;
+	private Fornecedor fornecedor;
+	private RepositorioFornecedorLista proximo;
 
-		public RepositorioFornecedorLista() {
-			this.fornecedor = null;
-			this.proximo = null;
-		}
-	
+	public RepositorioFornecedorLista() {
+		this.fornecedor = null;
+		this.proximo = null;
+	}
 
-		// negocios cadastro (checa se tem ou n„o antes de ir pros metodos);
-		//
-
-		public void inserir(Fornecedor forn) throws FornecedorNomeJaCadastradoException { // todo q for criado vai ter
-			if (!existir(forn)) { // forn. e novo fornecedor
-				if (this.fornecedor.equals(null)) {
-					this.fornecedor = forn;
-					this.proximo = new RepositorioFornecedorLista();
-				} else {
-					this.proximo.inserir(forn);
-				}
+	public void inserir(Fornecedor forn)
+			throws FornecedorNomeJaCadastradoException { // todo q for criado
+															// vai ter
+			if (this.fornecedor == null) {
+				this.fornecedor = forn;
+				this.proximo = new RepositorioFornecedorLista();
 			} else {
-				throw new FornecedorNomeJaCadastradoException();
-			}
+				this.proximo.inserir(forn);
 		}
+	}
 
-		public Fornecedor procurar(String cnpj) throws FornecedorNaoEncontradoException {
-			if (this.fornecedor.getCnpj().equals(cnpj)) {
-				return fornecedor;
-			} else if (this.proximo != null) {
-				return this.proximo.procurar(cnpj);
-			} else {
-				throw new FornecedorNaoEncontradoException();
-			}
+	public void atualizar(Fornecedor novo)
+			throws FornecedorNaoEncontradoException {
+		if (this.fornecedor != null && this.fornecedor.getCnpj().equals(novo.getCnpj())) {
+			this.fornecedor = novo;
+		} else if (this.proximo != null) {
+			this.proximo.atualizar(novo);
+		} else {
+			throw new FornecedorNaoEncontradoException();
 		}
+	}
 
-		public boolean existir(Fornecedor cnpj) {
-			if (this.fornecedor.getCnpj().equals(cnpj)) {
-				return true;
-			} else if (this.proximo != null) {
-				return this.proximo.existir(cnpj);
-			} else {
-				return false;
-			}
+	// negocios cadastro (checa se tem ou n√£o antes de ir pros metodos);
+	//
+
+	public Fornecedor procurar(String cnpj)  throws FornecedorNaoEncontradoException {
+		if (this.fornecedor != null && this.fornecedor.getCnpj().equals(cnpj)) {
+			return fornecedor;
+		} else if (this.proximo != null) {
+			return this.proximo.procurar(cnpj);
+		} else {
+			throw new FornecedorNaoEncontradoException();
 		}
+	}
 
-		// nesse atualizar tem que criar um metodo no main para mudar x para y, por
-		// exemplo
-
-		public void atualizar(Fornecedor novo) throws FornecedorNaoEncontradoException {
-			if (this.fornecedor.getCnpj().equals(novo.getCnpj())) {
-				this.fornecedor = novo;
-			} else if (this.proximo != null) {
-				this.proximo.atualizar(novo);
-			} else {
-				throw new FornecedorNaoEncontradoException();
-			}
+	public boolean existir(Fornecedor forn) {
+		if (this.fornecedor != null && this.fornecedor.getCnpj().equals(forn.getCnpj())) {
+			// If (this....existir(funcionario.getCnpj()))
+			return true;
+		} else if (this.proximo != null) {
+			return this.proximo.existir(forn);
+		} else {
+			return false;
 		}
+	}
 
-		public void remover(Fornecedor forn) throws FornecedorNaoEncontradoException {
-			if (this.fornecedor.getCnpj().equals(forn.getCnpj())) {
-				this.fornecedor = this.proximo.fornecedor;
-				this.proximo = this.proximo.proximo; // nessa linha ele gera um loop ate chegar no null?
+	public void remover(Fornecedor forn)
+			throws FornecedorNaoEncontradoException {
+		if (this.fornecedor != null
+				&& this.fornecedor.getCnpj().equals(forn.getCnpj())) {
+			this.fornecedor = this.proximo.fornecedor;
+			this.proximo = this.proximo.proximo; 
 
-			} else if (this.proximo != null) {
-				this.proximo.remover(forn);
-			} else {
-				throw new FornecedorNaoEncontradoException();
-			}
+		} else if (this.proximo != null) {
+			this.proximo.remover(forn);
+		} else {
+			throw new FornecedorNaoEncontradoException();
 		}
-	
-
-	
-	
-	/*
-	public String alarmeInicio(Cantina cantina) {
-	String aviso = cantina.getNome() + " acabou de abrir! :)";
-	return aviso;
-}
-
-public String alarmeFim(Cantina cantina) {
-	String aviso = cantina.getNome() + " acabou de fechar! :(";
-	return aviso;
-}
-
-}
-
-*/
+	}
 
 }
